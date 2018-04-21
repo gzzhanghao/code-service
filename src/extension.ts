1e24d394bd74aeb7f178e35abf5c2174c46dea29
Jason <jason.daurus@gmail.com>
2018-04-21 16:38:19 +0800

    inital commit

---
+++ src/extension.ts
@@ -0,0 +1,88 @@
+import * as os from 'os'
+import * as ws from 'ws'
+import * as path from 'path'
+import * as vscode from 'vscode'
+
+let config = vscode.workspace.getConfiguration('previewserver')
+let server: ws.Server|null = null
+
+export function activate(context: vscode.ExtensionContext) {
+    log('activate')
+    restartServer()
+
+    vscode.workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration('previewserver')) {
+            log('configuration changed, reload configs')
+            config = vscode.workspace.getConfiguration('previewserver')
+        }
+        if (event.affectsConfiguration('previewserver.serverPort')) {
+            log('server port changed, restart server')
+            restartServer()
+        }
+    })
+}
+
+export function deactivate() {
+    log('deactivate')
+    closeServer()
+}
+
+function restartServer() {
+    closeServer()
+
+    log('starting server')
+
+    server = new ws.Server({ port: config.serverPort })
+
+    server.on('listening', () => {
+        log('server listening at port', config.serverPort)
+    })
+
+    server.on('error', error => {
+        log('server error:', error.stack)
+    })
+
+    server.on('connection', socket => {
+        log('connection established')
+
+        socket.on('error', error => {
+            log('connection error:', error.stack)
+        })
+
+        socket.on('message', async data => {
+            try {
+                log('message received', data)
+                const msg = JSON.parse(data.toString())
+
+                let basePath = config.basePaths || os.homedir()
+                if ('project' in msg) {
+                    basePath = basePath[msg.project]
+                }
+
+                const uri = vscode.Uri.file(path.join(basePath, msg.path))
+                const doc = await vscode.workspace.openTextDocument(uri)
+
+                await vscode.window.showTextDocument(doc, msg.options)
+            } catch (error) {
+                log('open file error:', error.stack)
+            }
+        })
+    })
+}
+
+function closeServer() {
+    if (!server) {
+        return
+    }
+    log('closing server')
+    server.removeAllListeners()
+    server.close()
+    server = null
+}
+
+function log(...args: any[]) {
+    if (config.debug) {
+        console.log('[preview-server]', ...args)
+        vscode.debug.activeDebugConsole.appendLine(`[preview-server] ${args.join(' ')}`)
+    }
+}
